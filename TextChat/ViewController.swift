be9f44b186ff61f3d77ff3577a394421b6fd2802
Garika Sreekanth <sreekanthgarika91@gmail.com>
2020-12-11 18:53:51 +0530

    Initial Commit

---
+++ TextChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextChat
+//
+//  Created by Garika Sreekanth on 11/12/20.
+//  Copyright © 2020 Garika Sreekanth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
